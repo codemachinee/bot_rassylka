19b5dce0140964534cdb6269c30a7e08504f610a
codemachinee <johnybritva2@gmail.com>
2023-06-06 22:45:16 +0300

    добавлена запись в базу

--- functions.py
+++ functions.py
@@ -5,6 +5,8 @@ from datetime import *
 # библиотека рандома
 from random import *
 from passwords import *
+import pytz
+tz_moscow = pytz.timezone('Europe/Moscow')
 
 admin_account = igor
 
@@ -18,96 +20,90 @@ class model_buttons: # класс формирования клавиатур
 
     async def rasylka_buttons(self):
         kb5 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-        but1 = types.KeyboardButton(text='база1')
-        but2 = types.KeyboardButton(text='база2')
-        but3 = types.KeyboardButton(text='база3')
-        kb5.add(but1, but2, but3)
+        but1 = types.KeyboardButton(text='база общая')
+        but2 = types.KeyboardButton(text='база рассылки')
+        kb5.add(but1, but2)
         await self.bot.send_message(self.message.chat.id, f'Выберите базу для рассылки', reply_markup=kb5)
+
+    async def otmena_button(self):
+        kb3 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+        but1 = types.KeyboardButton(text='Отмена')
+        kb3.add(but1)
+        return kb3
+
 
 
 class clients_base:  # класс базы данных
 
-    def __init__(self, bot, message, message_id, auto_model, base=None):
+    def __init__(self, bot, message, message_id=None, base=None):
         self.bot = bot
         self.message = message
         self.message_id = message_id
-        self.auto_model = auto_model
         self.base = base
         gc = gspread.service_account(filename='base_key.json')  # доступ к гугл табл по ключевому файлу аккаунта разраба
         # открытие таблицы по юрл адресу:
         sh = gc.open('base')
-        self.worksheet = sh.worksheet('база1')  # выбор листа 'общая база клиентов' таблицы
-        self.worksheet2 = sh.worksheet('база2')
-        self.worksheet3 = sh.worksheet('база3')
+        self.worksheet = sh.worksheet('база общая')  # выбор листа 'общая база клиентов' таблицы
+        self.worksheet2 = sh.worksheet('база рассылки')
 
-    # async def chec_and_record(self):  # функция поиска и записи в базу
-    #     worksheet_len = len(self.worksheet.col_values(1)) + 1  # поиск первой свободной ячейки для записи во 2 столбце
-    #     worksheet_len2 = len(self.worksheet2.col_values(1)) + 1
-    #     await self.bot.send_message(admin_account, 'Пробиваю базу..')
-    #     await self.bot.send_message(admin_account, '...')
-    #     if str(self.message.chat.id) in self.worksheet.col_values(1):
-    #         await self.bot.send_message(admin_account, ' Клиент есть в базе')
-    #     else:
-    #         await self.bot.send_message(admin_account, f'Клиент добавлен в базу\n'
-    #                 f'База: '
-    #                 f'https://docs.google.com/spreadsheets/d/1M3PHqj06Ex1_oXKuyR8CZCjl4j67qxvQUNNfcA3WjyY/edit#gid=0')
-    #         self.worksheet.update(f'A{worksheet_len}:F{worksheet_len}', [[self.message.chat.id, self.message.from_user.username,
-    #                               self.message.from_user.first_name, self.message.from_user.last_name,
-    #                               self.auto_model, str(datetime.now().date())]])
-    #         self.worksheet2.update(f'A{worksheet_len2}:F{worksheet_len2}',
-    #                               [[self.message.chat.id, self.message.from_user.username,
-    #                                 self.message.from_user.first_name, self.message.from_user.last_name,
-    #                                 self.auto_model, str(datetime.now().date())]])
+    async def chec_and_record(self, channel, wallet):  # функция поиска и записи в базу
+        global tz_moscow
+        worksheet_len = len(self.worksheet.col_values(1)) + 1  # поиск первой свободной ячейки для записи во 2 столбце
+        worksheet_len2 = len(self.worksheet2.col_values(1)) + 1
+        # await self.bot.send_message(admin_account, 'Пробиваю базу..')
+        # await self.bot.send_message(admin_account, '...')
+        # if str(self.message.chat.id) in self.worksheet.col_values(1):
+        #     await self.bot.send_message(admin_account, ' Клиент есть в базе')
+        # else:
+        #     await self.bot.send_message(admin_account, f'Клиент добавлен в базу\n'
+        #             f'База: '
+        #             f'https://docs.google.com/spreadsheets/d/1M3PHqj06Ex1_oXKuyR8CZCjl4j67qxvQUNNfcA3WjyY/edit#gid=0')
+        self.worksheet.update(f'A{worksheet_len}:F{worksheet_len}', [[self.message.chat.id, self.message.from_user.username,
+                              channel, wallet, self.message.from_user.first_name, str(datetime.now(tz_moscow).date())]])
+        self.worksheet2.update(f'A{worksheet_len2}:F{worksheet_len2}', [[self.message.chat.id,
+                                                                       self.message.from_user.username, channel,
+                                                                       wallet, self.message.from_user.first_name,
+                                                                       str(datetime.now(tz_moscow).date())]])
+        await self.bot.send_message(admin_account, f'Клиент @{self.message.from_user.username} добавлен в базу\n'
+                                                   f'База: '
+                                                   f'https://docs.google.com/spreadsheets/d/1CrhViB9dhtaAxcyu_n3S1l0UmpyRecTH9PCV5Lon0_U/edit?usp=sharing')
 
-    # async def perevod_v_bazu(self):  # функция перевода из базы потенциальных клиентов в базу старых клиентов
-    #     try:
-    #         worksheet_len3 = len(self.worksheet3.col_values(1)) + 1
-    #         cell = self.worksheet.find(self.perehvat)  # поиск ячейки с данными по ключевому слову
-    #         # запись клиента в свободную строку базы старых клиентов:
-    #         self.worksheet3.update(f'A{worksheet_len3}:F{worksheet_len3}', [self.worksheet.row_values(cell.row)])
-    #         self.worksheet2.batch_clear([f"A{cell.row}:F{cell.row}"])  # удаление клиента из базы потенциальных
-    #         await self.bot.send_message(admin_account, 'Птичка в клетке ✅')
-    #     except AttributeError:
-    #         await self.bot.send_message(admin_account, 'Ошибка, пользователь отсутствует, будь внимательнее если осознал свой '
-    #                                             'косяк воспользуйся командой /next_level_base снова')
+    async def perevod_v_bazu(self):  # функция перевода из базы потенциальных клиентов в базу старых клиентов
+        try:
+            worksheet_len3 = len(self.worksheet3.col_values(1)) + 1
+            cell = self.worksheet.find(self.perehvat)  # поиск ячейки с данными по ключевому слову
+            # запись клиента в свободную строку базы старых клиентов:
+            self.worksheet3.update(f'A{worksheet_len3}:F{worksheet_len3}', [self.worksheet.row_values(cell.row)])
+            self.worksheet2.batch_clear([f"A{cell.row}:F{cell.row}"])  # удаление клиента из базы потенциальных
+            await self.bot.send_message(admin_account, 'Птичка в клетке ✅')
+        except AttributeError:
+            await self.bot.send_message(admin_account, 'Ошибка, пользователь отсутствует, будь внимательнее если осознал свой '
+                                                'косяк воспользуйся командой /next_level_base снова')
 
     async def rasylka_v_bazu(self):  # функция рассылки постов в базы
         kb5 = types.ReplyKeyboardRemove()  # удаление клавиатуры
         kb6 = types.InlineKeyboardMarkup(row_width=1)
         but1 = types.InlineKeyboardButton(text='Конечно!', callback_data='btn')
         kb6.add(but1)
-        if self.base == 'база1':
+        if self.base == 'база общая':
             for i in range(1, len(self.worksheet.col_values(1))):
                 try:
                     await self.bot.copy_message(self.worksheet.col_values(1)[i], self.message.chat.id,
                                                 self.message_id, reply_markup=kb5)
-                    #self.bot.send_message(self.worksheet.col_values(1)[i], 'Участвовать в акции?', reply_markup=kb6)
                 except Exception:
                     await self.bot.send_message(admin_account, f'Босс, похоже @{self.worksheet.col_values(2)[i]} меня '
                                                                f'заблокировал. Рассылка невозможна.', reply_markup=kb5)
-            await self.bot.edit_message_text(f'Босс, рассылка в базу1 выполнена ✅', self.message.chat.id,
+            await self.bot.edit_message_text(f'Босс, рассылка в общую базу выполнена ✅', self.message.chat.id,
                                              message_id=self.message.message_id + 1)
-        if self.base == 'база2':
+        if self.base == 'база рассылки':
             for i in range(1, len(self.worksheet.col_values(1))):
                 try:
                     await self.bot.copy_message(self.worksheet2.col_values(1)[i], self.message.chat.id,
                                                 self.message_id, reply_markup=kb5)
-                    #self.bot.send_message(self.worksheet2.col_values(1)[i], 'Участвовать в акции?', reply_markup=kb6)
                 except Exception:
                     await self.bot.send_message(self.message.chat.id, f'Босс, похоже @{self.worksheet2.col_values(2)[i]} '
                                                                       f'заблочил меня. Рассылка невозможна',
                                                 reply_markup=kb5)
-            await self.bot.edit_message_text(f'Босс, рассылка в базу2 выполнена ✅', self.message.chat.id,
+            await self.bot.edit_message_text(f'Босс, рассылка в базу рассылки выполнена ✅', self.message.chat.id,
                                              message_id=self.message.message_id + 1)
-        if self.base == 'база3':
-            for i in range(1, len(self.worksheet.col_values(1))):
-                try:
-                    await self.bot.copy_message(self.worksheet3.col_values(1)[i], self.message.chat.id,
-                                                self.message_id, reply_markup=kb5)
-                    #self.bot.send_message(self.worksheet3.col_values(1)[i], 'Участвовать в акции?', reply_markup=kb6)
-                except Exception:
-                    await self.bot.send_message(self.message.chat.id, f'Босс, похоже @{self.worksheet2.col_values(2)[i]} '
-                                                                      f'заблочил меня. Рассылка невозможна',
-                                                reply_markup=kb5)
-            await self.bot.edit_message_text(f'Босс, рассылка в базу3 выполнена ✅', self.message.chat.id,
-                                             message_id=self.message.message_id + 1)
+
--- main.py
+++ main.py
@@ -22,6 +22,8 @@ dp = Dispatcher(bot, storage=storage)
 class Form(StatesGroup):
     text = State()
     message_id = State()
+    channel = State()
+    wallet = State()
 
 
 rasylka = None
@@ -29,15 +31,18 @@ rasylka = None
 
 @dp.message_handler(commands=['start'])    # перехватчик команды /start
 async def start(message):
-    kb2 = types.ReplyKeyboardRemove()
-    await bot.send_message(message.chat.id, f'Приветственное сообщение\n'
-                                            f'/post - устроить рассылку\n'
-                                            f'/help - все возможности бота', reply_markup=kb2)
+    await bot.send_message(message.chat.id, f"Welcome to the bookmaker.XYZ freebet bot! We're excited to have you "
+                                            f"here. To get started, please send us the necessary information so "
+                                            f"that we can give you your free bet. We can't wait to see you start "
+                                            f"winning!\n\nCan you tell me which channel you're coming from? Just "
+                                            f"type in the name below, please. 😊",
+                           reply_markup=await model_buttons(bot, message).otmena_button())
     await bot.delete_message(message.chat.id, message.message_id)
     try:
         await bot.delete_message(message.chat.id, message.message_id - 1)
     except Exception:
         pass
+    await Form.channel.set()
 
 
 @dp.message_handler(commands=['help'])
@@ -53,7 +58,8 @@ async def help(message):
 
 @dp.message_handler(commands=['post'])
 async def post(message):
-    await bot.send_message(message.chat.id, f'Введите текст/фото/видео/файл для рассылки и отправьте мне..')
+    await bot.send_message(message.chat.id, f'Введите текст/фото/видео/файл для рассылки и отправьте мне..',
+                           reply_markup=await model_buttons(bot, message).otmena_button())
     await bot.delete_message(message.chat.id, message.message_id)
     try:
         await bot.delete_message(message.chat.id, message.message_id - 1)
@@ -64,19 +70,61 @@ async def post(message):
 
 @dp.message_handler(state=Form.text, content_types=['text', 'document', 'photo', 'video'])  # Принимаем состояние
 async def new_message(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:  # Устанавливаем состояние ожидания
-        data['text'] = message.message_id
-        await model_buttons(bot, message).rasylka_buttons()
-        await Form.message_id.set()
+    if message.text == 'Отмена':
+        kb2 = types.ReplyKeyboardRemove()
+        await state.finish()
+        await bot.send_message(message.chat.id, f'Действие отменено', reply_markup=kb2)
+        await bot.delete_message(message.chat.id, (message.message_id - 1))
+        await bot.delete_message(message.chat.id, message.message_id)
+    else:
+        async with state.proxy() as data:  # Устанавливаем состояние ожидания
+            data['text'] = message.message_id
+            await model_buttons(bot, message).rasylka_buttons()
+            await Form.message_id.set()
 
 
 @dp.message_handler(state=Form.message_id)  # Принимаем состояние
 async def new_message(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:  # Устанавливаем состояние ожидания
-        await bot.send_message(message.chat.id, 'рассылаю сообщение..⏳')
-        await clients_base(bot, message, data['text'], auto_model=None, base=message.text).rasylka_v_bazu()
+    if message.text == 'Отмена':
+        kb2 = types.ReplyKeyboardRemove()
         await state.finish()
+        await bot.send_message(message.chat.id, f'Действие отменено', reply_markup=kb2)
+        await bot.delete_message(message.chat.id, (message.message_id - 1))
+        await bot.delete_message(message.chat.id, message.message_id)
+    else:
+        async with state.proxy() as data:  # Устанавливаем состояние ожидания
+            await bot.send_message(message.chat.id, 'рассылаю сообщение..⏳')
+            await clients_base(bot, message, data['text'], base=message.text).rasylka_v_bazu()
+            await state.finish()
 
+
+@dp.message_handler(state=Form.channel)  # Принимаем состояние
+async def new_message(message: types.Message, state: FSMContext):
+    if message.text == 'Отмена':
+        kb2 = types.ReplyKeyboardRemove()
+        await state.finish()
+        await bot.send_message(message.chat.id, f'Действие отменено', reply_markup=kb2)
+        await bot.delete_message(message.chat.id, (message.message_id - 1))
+        await bot.delete_message(message.chat.id, message.message_id)
+    else:
+        async with state.proxy() as data:
+            kb2 = types.ReplyKeyboardRemove()# Устанавливаем состояние ожидания
+            data['channel'] = message.text
+            await bot.send_message(message.chat.id, f'Please give us the address of your Ethereum wallet.',
+                                   reply_markup=kb2)
+            await bot.delete_message(message.chat.id, (message.message_id - 1))
+            await bot.delete_message(message.chat.id, message.message_id)
+            await Form.wallet.set()
+
+
+@dp.message_handler(state=Form.wallet)  # Принимаем состояние
+async def new_message(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:  # Устанавливаем состояние ожидания
+        await bot.delete_message(message.chat.id, message.message_id)
+        await bot.edit_message_text('Thank you! We will send you a freebet soon if you meet the '
+                                    'necessary requirements.', message.chat.id, (message.message_id - 1))
+        await clients_base(bot, message).chec_and_record(data['channel'], message.text)
+        await state.finish()
 
 if __name__ == '__main__':
     # scheduler.add_job(statistic().obnulenie, "cron", day_of_week='mon-sun', hour=0)
